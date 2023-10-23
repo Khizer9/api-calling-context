9f72eb63da5dee5220e9679d0296b7b24d020adb
Khizer Ali <khizer358@gmail.com>
2023-10-23 22:30:56 +0500

    misc changes

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,17 +5,20 @@ const Home = () => {
 
     const {products, loading} = useProducts()   
 
+    const handleClick = () => {
+      console.log('clicked')
+    }
 
   return (
     <div className='main-container'>
         <div className='product-list'>
-      {loading ? <div class="lds-ripple"><div></div><div></div></div> : products.map((product) => (
+      {loading ? <div className="lds-ripple"><div></div><div></div></div> : products.map((product) => (
         <div className='product-card' key={product.id}>
           <img className='product-img' src={product.thumbnail} alt={product.title} />
           <h2>{product.title}</h2>
           <p>{product.description}</p>
           <p>Price: ${product.price}</p>
-          <button className='product-btn'>Add to cart</button>
+          <button className='product-btn' onClick={handleClick}>Add to cart</button>
         </div>
       ))}
       </div>
